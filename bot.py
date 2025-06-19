5903ac66d8653610fb49b5892e707abe8164e5e8
uriahn <85145706+uriahn@users.noreply.github.com>
2025-06-18 20:46:32 -0700

    Made it actually work
    I kinda rewrote most of it. Now it actually responds with an AI message instead of something along the lines of “It works!” or “TODO: Make this actually do something”

--- bot.py
+++ bot.py
@@ -9,6 +9,30 @@ load_dotenv()
 DISCORD_API_TOKEN = os.getenv("DISCORD_API_TOKEN")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
+# Defining OpenAI client
+openai_client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=OPENROUTER_API_KEY
+)
+
+async def get_ai_response(message_content):
+    try:
+        loop = asyncio.get_event_loop()
+        response = await loop.run_in_executor(
+            None, 
+            lambda: openai_client.chat.completions.create(
+                model="meta-llama/llama-4-maverick:free", # Just used this cause I felt like it and it's free ¯\_(ツ)_/¯. Also it's moderated but not too censored
+                messages=[{"role": "user", "content": message_content}],
+                extra_headers={
+                    "HTTP-Referer": "https://github.com/uriahn/konan-ai",
+                    "X-Title": "Konan AI",
+                }
+            )
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return f"Sorry, the horrible code of my developer caused this error: {str(e)}"
+
 # Just some basic Discord loading stuff
 intents = discord.Intents.default()
 intents.message_content = True
@@ -23,30 +47,15 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author == client.user: # Checks to make sure that the message wasn't sent by the bot
-        return 
-    elif client.user.mentioned_in(message) or message.channel.id == 1307212823356768326 or message.channel.id == 1372374099409502208: #TODO: Make this changable in a seperate file
-        await message.channel.send("TODO: Make this do anything at all")
+        return
+
+    if client.user.mentioned_in(message) or message.channel.id == 1307212823356768326 or message.channel.id == 1372374099409502208 or message.channel.id == 1111881291365883984: # TODO: Make this changable in a seperate file
+        user_message = message.content
+
+        if client.user.mentioned_in(message):
+            user_message = user_message.replace(f'<@{client.user.id}>', '').strip() # Removes the junk from the message
+
+        ai_response = await get_ai_response(user_message) # Send message to AI
+        await message.channel.send(ai_response) # Send AI response as a message
 
 client.run(DISCORD_API_TOKEN)
-
-# Defining OpenAI stuff
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=OPENROUTER_API_KEY
-)
-
-#TODO: Make this do anything at all
-# Sending message to the AI
-completion = client.chat.completions.create(
-    extra_headers={
-        "HTTP-Referer": "https://github.com/uriahn/konan-ai",
-        "X-Title": "Konan AI",
-    },
-    model="meta-llama/llama-4-maverick:free", # Just used this cause I felt like it and it's free ¯\_(ツ)_/¯. Also it's moderated but not too censored
-        messages=[
-        {
-            "role": "user",
-            "content": "This will be a variable eventually, haven't added the messages yet."
-        }
-    ]
-)
