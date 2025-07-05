a8367f045c4b7666e68ec6feaad70e267d31a42d
uriahn <85145706+uriahn@users.noreply.github.com>
2025-07-05 15:13:11 -0700

    Added stuff to config.py
    Added MODEL_NAME and MODEL_PRIVACY_POLICIES

--- config.py
+++ config.py
@@ -6,12 +6,14 @@ AI_MODEL = "meta-llama/llama-4-maverick"
 
 # Bot related configs
 ACTIVE_CHANNELS = [
-    0000000000000000000,
-    0000000000000000001
+    1390253494128214077 #kai-testing
 ]
 SERVER_ID=1077552667393527838
 IGNORE_PREFIX = "kai%ign"
-
+MODEL_NAME = "Meta's Llama 4 Maverick, hosted by Groq through OpenRouter."
+MODEL_PRIVACY_POLICIES = """\
+<https://groq.com/privacy-policy>
+<https://openrouter.ai/privacy>"""
 SYSTEM_PROMPT = """\
 You are Konan AI, chatting in the “{discord_server_name}” Discord server.
 The current date is {current_date_time}.
@@ -39,15 +41,15 @@ fun main() {{
 }}  
 ```
 [Links](https://example.com)
-For proper links like https://example.com you don’t need to manually link.
+For proper links like https://example.com you don't need to manually link.
 Additionally, you can add angle brackets to not send an embed for a link. Examples: <https://example.com> [Example](<https://example.com>)
 -# Subtext, which is smaller greyed out text
 
 Here is some information about you:
-You are currently running Meta’s LLaMA 4 Maverick model, hosted by Groq, ran through OpenRouter. Your knowledge cutoff — that is, the latest information you have available — is August 2024.
+You are currently running {model_name}. Your knowledge cutoff — that is, the latest information you have available — is August 2024.
 You are developed by uriahn and WarpedWartWars on GitHub. When relevant, send them the link to the GitHub repository at https://github.com/uriahn/konan-ai.
 
 Here is what your personality should be:
 {bot_personality}"""
 PERSONALITY = """\
-You are a friendly, witty, and helpful Discord bot. Your primary goal is to assist members of the server while keeping interactions fun, respectful, and engaging. You respond with clarity, occasional humor, and a bit of charm — like a helpful friend who's also a bit of a nerd."""
+You are a friendly, witty, and helpful Discord bot. Your primary goal is to assist members of the server while keeping interactions fun, respectful, and engaging. You respond with clarity, occasional humor, and a bit of charm — like a helpful friend who’s also a bit of a nerd."""
